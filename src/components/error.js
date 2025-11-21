8920f93bbb70e53d319ca094c01b453d3812d250
daavhidd <david.nwokeabia@lloydant.com>
2025-11-21 19:14:14 +0100

    updates

--- src/components/error.js
+++ src/components/error.js
@@ -8,7 +8,7 @@ export default function Error() {
       <div className="text-center">
         <h1 className="text-9xl font-bold text-[#ab92af]">404</h1>
         <p className="text-2xl md:text-3xl font-medium mt-4 text-white">Oops! Page not found</p>
-        <p className="text-gray-300 mt-2">The page you’re looking for doesn’t exist.</p>
+        <p className="text-gray-300 mt-2">The page you’re looking for doesn’t exist, go back home to the portfolio.</p>
         <Link to="/" className="mt-6 inline-flex px-12 py-2 bg-[#ab92af] text-white rounded-md shadow hover:bg-[#45154e] herobtn">
           <MdArrowBackIos className="mr-2 my-auto"/>
           Go Back Home
